5d40283739481b85cdae432267e8f0438be07f6a
Brice Cabioch <cabioch.brice@gmail.com>
2024-02-15 20:39:33 +0100

    iteration 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CountryDetails from "./pages/CountryDetailsPage";
+
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/CountryDetails" element={<CountryDetails />} />
+      </Routes>
     </div>
   );
 }
