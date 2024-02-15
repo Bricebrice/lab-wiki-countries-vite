762d5a661281393670d3f51f665defc5cd9a0169
Brice Cabioch <cabioch.brice@gmail.com>
2024-02-15 21:21:37 +0100

    iteration 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/CountryDetails" element={<CountryDetails />} />
+        <Route path="/:countryId" element={<CountryDetails />} />
       </Routes>
     </div>
   );
