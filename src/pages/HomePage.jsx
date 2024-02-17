9830618719b43217d12fee30cffbd20329197e26
Brice Cabioch <cabioch.brice@gmail.com>
2024-02-17 13:27:55 +0100

    refactoring and iteration 3 and 4 completed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,60 +1,55 @@
+// import { countries } from "../test/data.js";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
 function HomePage() {
+  const [foundCountries, setFoundCountry] = useState([]);
+
+  // useEffect(() => {
+  //   console.log("countries ", countries);
+  //   setFoundCountry(countries);
+  // }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://ih-countries-api.herokuapp.com/countries")
+      .then((response) => {
+        console.log("response.data: ", response.data);
+        setFoundCountry(response.data);
+        console.log("One country: ", response.data[0]);
+      });
+  }, []);
+
   return (
-    <div className="container" style={{ maxHeight: "90vh", overflow: scroll }}>
+    <div
+      className="container"
+      style={{ maxHeight: "90vh", overflow: "scroll" }}
+    >
       <h1 style={{ fontSize: "24px" }}>
         WikiCountries: Your Guide to the World
       </h1>
 
-      <div className="list-group">
-        <a className="list-group-item list-group-item-action" href="/ABW">
-          🇦🇼 Aruba
-        </a>
-        <a className="list-group-item list-group-item-action" href="/AFG">
-          🇦🇫 Afghanistan
-        </a>
-        <a className="list-group-item list-group-item-action" href="/AGO">
-          🇦🇴 Angola
-        </a>
-        <a className="list-group-item list-group-item-action" href="/AIA">
-          🇦🇮 Anguilla
-        </a>
-        <a className="list-group-item list-group-item-action" href="/ALA">
-          🇦🇽 Åland Islands
-        </a>
-        <a className="list-group-item list-group-item-action" href="/ALB">
-          🇦🇱 Albania
-        </a>
-        <a className="list-group-item list-group-item-action" href="/AND">
-          🇦🇩 Andorra
-        </a>
-        <a className="list-group-item list-group-item-action" href="/ARE">
-          🇦🇪 United Arab Emirates
-        </a>
-        <a className="list-group-item list-group-item-action" href="/ARG">
-          🇦🇷 Argentina
-        </a>
-        <a className="list-group-item list-group-item-action" href="/ARM">
-          🇦🇲 Armenia
-        </a>
-        <a className="list-group-item list-group-item-action" href="/ASM">
-          🇦🇸 American Samoa
-        </a>
-        <a className="list-group-item list-group-item-action" href="/ATA">
-          🇦🇶 Antarctica
-        </a>
-        <a className="list-group-item list-group-item-action" href="/FLK">
-          🇫🇰 Falkland Islands
-        </a>
-        <a
-          className="list-group-item list-group-item-action active"
-          href="/FRA"
-        >
-          🇫🇷 France
-        </a>
-        <a className="list-group-item list-group-item-action" href="/ZWE">
-          🇿🇼 Zimbabwe
-        </a>
-      </div>
+      {!foundCountries && <p>Countries not found!</p>}
+
+      {foundCountries && (
+        <div className="list-group">
+          {foundCountries.map((country) => (
+            <Link
+              key={country._id}
+              className="list-group-item list-group-item-action"
+              to={`/${country.alpha3Code}`}
+            >
+              <img
+                src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+              />
+              <br />
+              {country.name.common}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
